36011690b2f160f8355f4da50f29fa9cf1437304
makwanas <makwanas@oregonstate.edu>
2020-02-04 02:06:47 -0800

    Version 1.0.0

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const mongoose = require ('mongoose');
-const config =  require ('config');
+const mongoose = require('mongoose');
+const config = require('config');
+const path = require('path');
 
-const app = express ();
+const app = express();
 
 //Body Parser Middleware
 
@@ -14,19 +15,29 @@ const db = config.get('mongoURI');
 
 //Connect to MonogoDB
 mongoose
-    .connect(db,{
+    .connect(db, {
         useNewUrlParser: true,
         useCreateIndex: true
     }) // Adding new Mongo url parser
     .then(() => console.log('MongoDB connected...'))
-    .catch(err => console.log(err) );
+    .catch(err => console.log(err));
 
 //Use Routes
 
-app.use('/api/events',require('./routes/api/events'));
-app.use('/api/users',require('./routes/api/users'));
-app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/events', require('./routes/api/events'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
+//Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
+
+}
 
 const port = process.env.PORT || 5000;
 
